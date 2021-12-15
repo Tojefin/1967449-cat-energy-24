70f5a2f9a53c5833e1bb82407ce49e6677dd8421
Tojefin <tojefin@gmail.com>
2021-12-16 01:19:38 +0400

    menu fix

--- source/js/menu-toggle.js
+++ source/js/menu-toggle.js
@@ -1,3 +1,5 @@
+window.onload = toggleMenu();
+
 function toggleMenu(){
   document.getElementById("menu-button").classList.toggle("menu__button--opened");
   document.getElementById("menu-list").classList.toggle("menu__list--open");
